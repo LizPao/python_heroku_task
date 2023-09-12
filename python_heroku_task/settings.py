cda182f89c176503b661bbef3f66e198174985bf
Paola Garcia <lizpaogarcia2@gmail.com>
2023-09-12 01:45:19 -0400

    new

--- python_heroku_task/settings.py
+++ python_heroku_task/settings.py
@@ -9,9 +9,11 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-import os
+from dotenv import load_dotenv
+load_dotenv()
+from os import environ
+
 from pathlib import Path
-import dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,12 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-dotenv_file = os.path.join(BASE_DIR, "env")
-if os.path.isfile(dotenv_file):
-    dotenv.load_dotenv(dotenv_file)
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -72,7 +71,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'python_heroku_task.wsgi.application'
+WSGI_APPLICATION = 'python_heroku_task.wsgi.app'
 
 
 # Database
@@ -80,12 +79,20 @@ WSGI_APPLICATION = 'python_heroku_task.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.environ['ENGINE'],
-        'NAME':  os.environ['NAME'],
-        'USER':  os.environ['USER'],
-        'PASSWORD':  os.environ['PASSWORD'],
-        'HOST':  os.environ['HOST'],
-        'PORT':  os.environ['PORT'],
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        # el host de tu base de datos
+        'HOST': environ.get('SUPABASE_HOST'),
+        # la contraseña que utilizaste al crear el proyecto
+        'PASSWORD': environ.get('SUPABASE_PASSWORD'),
+        'PORT': '5432',
+        # estas opciones se pasan a postgres, es opcional, pero recomendado
+        'OPTIONS': {
+            'sslmode': 'verify-full',
+            # el certificado que te de SUPABASE
+            'sslrootcert': Path.joinpath(BASE_DIR, 'prod-ca-2021.crt')
+        }
     }
 }
 
